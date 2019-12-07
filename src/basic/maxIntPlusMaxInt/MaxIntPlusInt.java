32c97404c36c12ec19a206cb95360d53d4fe1d86
„Tdecazgit config --global user.email „tadas.kaluina@gmail.com <tadas.kaluina@gmail.com>
2019-12-07 15:11:57 +0200

    max int plus

---
+++ src/basic/maxIntPlusMaxInt/MaxIntPlusInt.java
@@ -0,0 +1,12 @@
+package basic.maxIntPlusMaxInt;
+
+public class MaxIntPlusInt {
+
+    public static void main(String[] args) {
+
+        int x = Integer.MAX_VALUE;
+        long xx = x;
+        long y = xx + xx;
+        System.out.println(y);
+    }
+}
